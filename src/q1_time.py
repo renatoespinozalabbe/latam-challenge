973b6c0abcb219859047b48d47c4e9fcf25b16fa
027renato <renatoespinozalabbe@gmail.com>
2024-06-22 17:13:18 -0400

    feat: Add credentials path and dataframe creation on q1_time

--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,15 @@
 from typing import List, Tuple
 from datetime import datetime
+import os
+import pandas as pd
+
 
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
+
+    # Ruta a archivo de credenciales JSON de Google Cloud
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "../credentials/project-latam-challenge-749ce1a96052.json"
+
+    # Leer el archivo CSV en un DataFrame
+    df = pd.read_json(file_path,lines=True)
+
     pass
